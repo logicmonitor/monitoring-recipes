4925cb257fefd395c990c35cadd9bb15df26e720
Andrey Kitsen <andrey.kitsen@logicmonitor.com>
2020-07-16 12:01:18 -0700

    bits of clean up and new POST w/ GET example

--- DataSources/Groovy/SNMP/SNMP_Get.groovy
+++ DataSources/Groovy/SNMP/SNMP_Get.groovy
@@ -1,40 +1,28 @@
 /*******************************************************************************
- *  © 2007-2019 - LogicMonitor, Inc. All rights reserved.
+ *  © 2007-2020 - LogicMonitor, Inc. All rights reserved.
  ******************************************************************************/
 
-import com.santaba.agent.groovyapi.snmp.Snmp;
+import com.santaba.agent.groovyapi.snmp.Snmp
 
 // Set environment variables.
-def hostname = hostProps.get('system.hostname');
+def hostname = hostProps.get('system.hostname')
 def props = hostProps.toProperties()
 def snmp_timeout = 10000
 
 // Set SNMP OID.
-def snmp_oid = 'INSERT_OID_HERE'; // i.e. : 1.3.6.1.2.1.1.2
+def snmp_oid = 'INSERT_OID_HERE' // i.e. : 1.3.6.1.2.1.1.2
 
-// Try the following code.
-try {
-    /*
-    The following SNMP GET will handle v1 , v2 and v3. 
-    Props contains a map of ALL host properties and the SNMP GET method will automatically
-    handle the proper connection based on which SNMP version is configured.
-    */
+/*
+The following SNMP GET will handle v1 , v2 and v3.
+Props contains a map of ALL host properties and the SNMP GET method will automatically
+handle the proper connection based on which SNMP version is configured.
+*/
 
-    // Create SNMP GET with defined variables and map.
-    sampleGet = Snmp.get(hostname, snmp_oid, props, snmp_timeout);
+// Create SNMP GET with defined variables and map.
+sampleGet = Snmp.get(hostname, snmp_oid, props, snmp_timeout)
 
-    // print out the SNMP GET value.
-    println sampleGet
+// print out the SNMP GET value.
+println sampleGet
 
-    // Successful script execution, return 0;
-    return 0;
-}
-
-// Catch any exception that may have occurred.
-catch (Exception e) {
-    // Print the exception.
-    println e
-
-    // Exit with return code 1;
-    return 1;
-}
+// Successful script execution, return 0;
+return 0
--- DataSources/Groovy/SNMP/SNMP_Walk.groovy
+++ DataSources/Groovy/SNMP/SNMP_Walk.groovy
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  © 2007-2019 - LogicMonitor, Inc. All rights reserved.
+ *  © 2007-2020 - LogicMonitor, Inc. All rights reserved.
  ******************************************************************************/
 
 import com.santaba.agent.groovyapi.snmp.Snmp
@@ -12,38 +12,25 @@ def snmp_timeout = 10000
 // Set SNMP OID.
 def snmp_oid = 'INSERT_OID_HERE' // i.e. : 1.3.6.1.2.1.1.2
 
-// Try the following code.
-try {
+/*
+The following SNMP walk will handle v1 , v2 and v3.
+Props contains a map of ALL host properties and the SNMP walk method will automatically
+handle the proper connection based on which SNMP version is configured.
+*/
+
+// Create SNMP walk with defined variables and map.
+sampleWalk = Snmp.walk(hostname, snmp_oid, props, snmp_timeout)
+
+// Iterate through each line of the snmp output
+sampleWalk.eachLine() { line ->
+
+    // split on the '=' sign and get the oid and value.
+    (oid, value) = line.split(/ = /, 2)
+
     /*
-    The following SNMP walk will handle v1 , v2 and v3. 
-    Props contains a map of ALL host properties and the SNMP walk method will automatically
-    handle the proper connection based on which SNMP version is configured.
+     * Data post processing code goes here.
     */
-
-    // Create SNMP walk with defined variables and map.
-    sampleWalk = Snmp.walk(hostname, snmp_oid, props, snmp_timeout)
-
-    // Iterate through each line of the snmp output
-    sampleWalk.eachLine()
-            { line ->
-
-                // split on the '=' sign and get the oid and value.
-                (oid, value) = line.split(/ = /, 2)
-
-                /*
-                Data post processing code goes here.
-                */
-            }
-
-    // Successful script execution, return 0;
-    return 0
 }
 
-// Catch any exception that may have occurred.
-catch (Exception e) {
-    // Print the exception.
-    println e
-
-    // Exit with return code 1;
-    return 1
-}
+// Successful script execution, return 0;
+return 0
