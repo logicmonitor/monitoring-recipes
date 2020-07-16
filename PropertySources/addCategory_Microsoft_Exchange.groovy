f10d48ac4a2911eb1fb2b09fc7a6ecba1a2ddcad
Andrey Kitsen <andrey.kitsen@logicmonitor.com>
2020-07-16 12:03:10 -0700

    clean up PSs

--- PropertySources/addCategory_Microsoft_Exchange.groovy
+++ PropertySources/addCategory_Microsoft_Exchange.groovy
@@ -1,45 +1,35 @@
 /*******************************************************************************
- *  © 2007-2019 - LogicMonitor, Inc. All rights reserved.
+ *  © 2007-2020 - LogicMonitor, Inc. All rights reserved.
  ******************************************************************************/
 
 import com.santaba.agent.groovyapi.win32.WMI
 
 def host = hostProps.get("system.hostname")
 
-try {
+// get a list of running services
+def service_list = WMI.queryAll(host, "select * from win32_service")
 
-    // get a list of running services
-    def service_list = WMI.queryAll(host, "select * from win32_service")
+def is_exchange = false
 
-    def is_exchange = false
+// enumerate each service as a map
+service_list.each { service_map ->
 
-    // enumerate each service as a map
-    service_list.each
-            { service_map ->
+    // enumerate each of the fields in this service map
+    service_map.each { key, value ->
 
-                // enumerate each of the fields in this service map
-                service_map.each
-                        { key, value ->
-
-                            // is this an exchange service?
-                            if ((key == "NAME") && value.contains("MSExchange")) {
-                                // yes, flag it
-                                is_exchange = true
-                            }
-                        }
-            }
-
-    // did we locate an exchange service?
-    if (is_exchange) {
-
-        // yes, add the exchange to system.categories
-        println "system.categories=MSExchange"
+        // is this an exchange service?
+        if ((key == "NAME") && value.contains("MSExchange")) {
+            // yes, flag it
+            is_exchange = true
+        }
     }
-
-    return 0
-
 }
-catch (Exception e) {
-    println e
-    return 1
+
+// did we locate an exchange service?
+if (is_exchange) {
+
+    // yes, add the exchange to system.categories
+    println "system.categories=MSExchange"
 }
+
+return 0
